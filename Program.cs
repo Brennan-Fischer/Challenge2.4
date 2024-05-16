b5f201e9355aa74dac7b8e3b52cafbab21efa606
Brennan <blfische92@gmail.com>
2024-05-16 21:30:36 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Challenge2._4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("This program accepts an input character c and input width w to create a triangle of decreasing width");
+            Console.WriteLine("Please input character to create triangle out of: ");
+            char myCharacter = Convert.ToChar(Console.ReadLine());
+            Console.WriteLine("Please input desired width of first row");
+            int myWidth = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < myWidth; i++)
+            {
+                Console.WriteLine(" ");
+                for (int j = 0; j < myWidth-i; j++)
+                {
+                    Console.Write(myCharacter);
+                }
+            }
+
+        }
+    }
+}
